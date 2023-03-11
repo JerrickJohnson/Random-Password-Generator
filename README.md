b0a9b23bcd3e0622c4c5175e335bced3e6fb243a
JerrickJohnson <jerrickjohnson88@gmail.com>
2023-03-11 16:21:39 -0600

    Updated All Files and Addes Image

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Password Generator Starter Code
+# Random Password Generator
+
+## Desctiption:
+The Random Password Generator Application will generate a random password with the users
+desired criteria. They will be prompted to  give a desired length for the password that must
+be a number between 8 and 128. Then the user is prompted with a series of prompts
+asking if they would like lowercase letters, uppercase letters, numbers and special
+symbols. They must choose at least 1 of the criteria if not it will not generate a 
+password. After making selections of the approved criteria the Random Password 
+Generator will generate a password with random characters that meet the criteria 
+given by the user at the length desired. 
+
+## Assets:
+
+The following image demonstrates the web application's appearance and functionality:
+
+![Screenshot of Random Password Generator](./assets/images/PasswordGenPic.PNG)
+
+
+## The URL of the deployed application:
+
+[Git Page](https://jerrickjohnson.github.io/Random-Password-Generator/)
+
+## The URL of the GitHub repository:
+
+[Git Hub](https://github.com/JerrickJohnson/Random-Password-Generator.git)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,101 @@
-// Assignment code here
+//Variables
 
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var lowercaseLettters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+var UpperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var Numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+var Symbols = " !#$%&'()*+,-]./:;<=>?@[^_`|}{~";
+var SymbolsArray = Symbols.split("");
+
+//function to get random Index
+function getRandomIndex(arr = []) {
+  var index = arr[Math.floor(Math.random() * arr.length)];
+  return index;
+}
+
+//Prompts from window
+function passwordPrompts() {
+  var lengthPrompt = window.prompt("How long would you like this password to be?");
+  var hasLowerCase = window.confirm("Would you like to have lowercase Letters?");
+  var hasUpperCase = window.confirm("Would you like to have Uppercase Letters?");
+  var hasNumbers = window.confirm("Would you like to have Numbers?");
+  var hasSymbols = window.confirm("Would you like to have Symbols?");
+  var parsedLength = Number.parseInt(lengthPrompt);
+  
+  //Check Criteria
+  if (Number.isNaN(parsedLength)) {
+    console.log("Input length is not a number", lengthPrompt, parsedLength);
+    window.alert("Enter valid length Entry");
+    return null;
+    
+  }
+
+  if (parsedLength < 8 || parsedLength > 128) {
+    console.log("Invalid Input Length! Must be 8 to 128", lengthPrompt, parsedLength);
+    window.alert("Invalid Input Length! Length must be between 8 and 128");
+    return null;
+    
+  }
+
+  if (!hasLowerCase && !hasUpperCase && !hasNumbers && !hasSymbols) {
+    console.log("Answer no to all confirms");
+    window.alert("Cannot build password Answered no to all confirms");
+    return null;
+    
+  }
+  
+  return {
+    lengthPrompt,
+    hasLowerCase,
+    hasUpperCase,
+    hasNumbers,
+    hasSymbols
+  }
+}
+
+//Function to generate password
+function generatePassword(){
+  var result = [];
+  var chars = [];
+  var prompts = passwordPrompts();
+
+  if (prompts.hasLowerCase) {
+    chars = chars.concat(lowercaseLettters);
+    console.log(chars);
+  }
+  
+  if (prompts.hasUpperCase) {
+    chars = chars.concat(UpperCaseLetters);
+    console.log(chars);
+  }
+
+  if (prompts.hasNumbers) {
+    chars = chars.concat(Numbers);
+    console.log(chars);
+  }
+
+  if (prompts.hasSymbols) {
+    chars = chars.concat(SymbolsArray);
+    console.log(chars);
+  }
+
+  
+  for (let i = 0; i < prompts.lengthPrompt; i++) {
+    const element = getRandomIndex(chars);
+    result.push(element);
+    console.log(element);
+  }
+  console.log(result);
+
+  return result.join("");
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
